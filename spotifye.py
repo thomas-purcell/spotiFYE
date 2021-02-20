e5006d93bf6a37f2e25aed57b3d66f692b589ae5
thomas-purcell <purcell.t@northeastern.edu>
2021-02-20 18:56:27 -0500

    Came up with comparison method, parsed data

--- spotifye.py
+++ spotifye.py
@@ -1,15 +1,10 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from track import Track
-from compare import compare
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="50aed94f72d4414cafa02eda20c45457",
-                                               client_secret="0ce320e6442c4e289b3b35d146f8de17",
-                                               redirect_uri="http://localhost:8080",
-                                               scope="user-read-recently-played"))
+from utils import compare, build_json
 
 
-def build_playlist(playlist):
+def build_playlist(playlist, sp):
     result = []
 
     for idx, item in enumerate(playlist['items']):
@@ -21,31 +16,43 @@ def build_playlist(playlist):
         album = track['album']['name']
         album_id = track['album']['id']
         genres = sp.artist(artist_id)['genres']
-        new_track = Track(song, song_id, artist, artist_id, album, album_id, genres)
+        album_cover = track['album']['images'][1]['url']
+        new_track = Track(song, song_id, artist, artist_id, album, album_id, genres, album_cover)
         result.append(new_track)
 
     return result
 
 
-results = sp.current_user_recently_played(limit=50)
-user_playlist = build_playlist(results)  # list of user's last 50 listened to tracks
+def analyze_user():
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="50aed94f72d4414cafa02eda20c45457",
+                                                   client_secret="0ce320e6442c4e289b3b35d146f8de17",
+                                                   redirect_uri="http://localhost:8080",
+                                                   scope="user-read-recently-played"))
 
-top_playlists = sp.category_playlists(category_id='toplists', country='us', limit=50)['playlists']
-url = top_playlists['items'][0]['external_urls']['spotify']
-top_playlist = sp.playlist(url)['tracks']  # top hits playlist from spotify
+    results = sp.current_user_recently_played(limit=50)
+    user_playlist = build_playlist(results, sp)  # list of user's last 50 listened to tracks
 
-comparison_playlist = build_playlist(top_playlist)
+    top_playlists = sp.category_playlists(category_id='toplists', country='us', limit=50)['playlists']
+    url = top_playlists['items'][0]['external_urls']['spotify']
+    top_playlist = sp.playlist(url)['tracks']  # today's top hits playlist from Spotify
 
-print(user_playlist)
-print(comparison_playlist)
+    comparison_playlist = build_playlist(top_playlist, sp)
 
-title_count, artist_count, album_count, genre_count, total_genres, titles, artists, albums, genres = compare(user_playlist, comparison_playlist)
+    title_count, artist_count, album_count, genre_count, total_genres, titles, artists, albums, genres, urls \
+        = compare(user_playlist, comparison_playlist)
 
-print("Song Compatibility: ", str(round(title_count / 50 * 100, 2)), '%')
-print("Artist Compatibility: ", str(round(artist_count / 50 * 100, 2)), '%')
-print("Album Compatibility: ", str(round(album_count / 50 * 100, 2)), '%')
-print("Genre Compatibility: ", str(round(genre_count / total_genres * 100, 2)), '%')
-print(titles)
-print(artists)
-print(albums)
-print(genres)
+    title_percent = str(round(title_count / 50 * 100, 2)) + "%"
+    artist_percent = str(round(artist_count / 50 * 100, 2)) + "%"
+    album_percent = str(round(album_count / 50 * 100, 2)) + "%"
+    genre_percent = str(round(genre_count / total_genres * 100, 2)) + "%"
+
+    return build_json(title_percent, artist_percent, album_percent, genre_percent,
+                      titles, artists, albums, genres, urls)
+
+
+def main():
+    print(analyze_user())
+
+
+if __name__ == "__main__":
+    main()
--- track.py
+++ track.py
@@ -1,5 +1,5 @@
 class Track(object):
-    def __init__(self, song, song_id, artist, artist_id, album, album_id, genres):
+    def __init__(self, song, song_id, artist, artist_id, album, album_id, genres, album_cover):
         self.song = song
         self.song_id = song_id
         self.artist = artist
@@ -8,9 +8,12 @@ class Track(object):
         self.album_id = album_id
         self.genres = genres
         self.total_genres = len(genres)
+        self.album_cover = album_cover
 
     def __repr__(self):
-        return [self.song, self.song_id, self.artist, self.artist_id, self.album, self.album_id].__str__()
+        return [self.song, self.song_id, self.artist, self.artist_id,
+                self.album, self.album_id, self.genres, self.album_cover].__str__()
 
     def __str__(self):
-        return [self.song, self.song_id, self.artist, self.artist_id, self.album, self.album_id].__str__()
+        return [self.song, self.song_id, self.artist, self.artist_id,
+                self.album, self.album_id, self.genres, self.album_cover].__str__()
