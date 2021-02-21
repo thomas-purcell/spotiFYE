8602c3d3f5b73f409591f5e6a2d9bb0a30a7f606
thomas-purcell <purcell.t@northeastern.edu>
2021-02-21 00:30:48 -0500

    now comparing short_term top tracks to long_term top tracks

--- spotifye.py
+++ spotifye.py
@@ -6,20 +6,21 @@ from utils import compare, build_json
 
 def build_playlist(playlist, sp):
     result = []
-
-    for idx, item in enumerate(playlist['items']):
-        track = item['track']
-        song = track['name']
-        song_id = track['id']
-        artist = track['artists'][0]['name']
-        artist_id = track['artists'][0]['id']
-        album = track['album']['name']
-        album_id = track['album']['id']
-        genres = sp.artist(artist_id)['genres']
-        album_cover = track['album']['images'][1]['url']
-        new_track = Track(song, song_id, artist, artist_id, album, album_id, genres, album_cover)
+    for item in playlist['items']:
+        song = item['name']
+        song_id = item['uri']
+        artists = []
+        artists_id = []
+        genres = set()
+        for artist in item['artists']:
+            artists.append(artist['name'])
+            artists_id.append(artist['id'])
+            genres.update(sp.artist(artist['id'])['genres'])
+        album = item['album']['name']
+        album_id = item['album']['id']
+        album_cover = item['album']['images'][1]['url']
+        new_track = Track(song, song_id, artists, artists_id, album, album_id, genres, album_cover)
         result.append(new_track)
-
     return result
 
 
@@ -27,24 +28,25 @@ def analyze_user():
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="50aed94f72d4414cafa02eda20c45457",
                                                    client_secret="0ce320e6442c4e289b3b35d146f8de17",
                                                    redirect_uri="http://localhost:8080",
-                                                   scope="user-read-recently-played"))
+                                                   scope="user-read-recently-played, user-top-read"))
 
-    results = sp.current_user_recently_played(limit=50)
-    user_playlist = build_playlist(results, sp)  # list of user's last 50 listened to tracks
+    short_playlist = sp.current_user_top_tracks(limit=50, time_range='short_term')
+    short_term = build_playlist(short_playlist, sp)
 
-    top_playlists = sp.category_playlists(category_id='toplists', country='us', limit=50)['playlists']
-    url = top_playlists['items'][0]['external_urls']['spotify']
-    top_playlist = sp.playlist(url)['tracks']  # today's top hits playlist from Spotify
+    long_playlist = sp.current_user_top_tracks(limit=50, time_range='long_term')
+    long_term = build_playlist(long_playlist, sp)
 
-    comparison_playlist = build_playlist(top_playlist, sp)
+    title_count, artist_count, total_artists, album_count, genre_count, total_genres, titles, artists, albums, genres, \
+        urls = compare(short_term, long_term)
 
-    title_count, artist_count, album_count, genre_count, total_genres, titles, artists, albums, genres, urls \
-        = compare(comparison_playlist, user_playlist)
+    print(genre_count)
+    print(total_genres)
 
     title_percent = str(round(title_count / 50 * 100, 2)) + "%"
-    artist_percent = str(round(artist_count / 50 * 100, 2)) + "%"
+    artist_percent = str(round(artist_count / total_artists * 100, 2)) + "%"
     album_percent = str(round(album_count / 50 * 100, 2)) + "%"
     genre_percent = str(round(genre_count / total_genres * 100, 2)) + "%"
+
     return build_json(title_percent, artist_percent, album_percent, genre_percent,
                       titles, artists, albums, genres, urls)
 
